535351172170397485d08e0b13970faeefb75195
sherdos <sherdos26@gmail.com>
2024-03-09 00:54:58 +0600

    add navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,14 @@
+import Hello from "./components/Hello";
+import MyNavbar from "./components/MyNavbar";
 function App() {
   return (
-    <div className="App">Hello world</div>
-  )
+    <>
+      <div className="App">
+        <MyNavbar />
+        <Hello name="Sherdos" message="Hi" />
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/Hello.jsx
@@ -0,0 +1,9 @@
+function Hello(props) {
+  return (
+    <h1>
+      {props.message} {props.name}
+    </h1>
+  );
+}
+
+export default Hello;
